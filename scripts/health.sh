a2e36c72ed09ffcd24e9ec289f50986fdad9c871
mhdbk <50269616+mhdbk@users.noreply.github.com>
2026-01-15 19:19:48 +0530

    Update health check URL to use HOST variable

--- scripts/health.sh
+++ scripts/health.sh
@@ -4,9 +4,8 @@ set -euo pipefail
 if [[ $1 == "celery" ]]; then
   pgrep -f 'celery.*worker' && pgrep -f 'celery.*beat' && celery -A core inspect ping -t 10 || exit 1
 elif [ $1 == "server" ]; then
-  curl -sSf http://localhost:$PORT/healthcheck/ || exit 1
+  curl -fsSLk http://$HOST/healthcheck/ || exit 1
 else
   echo "Unknown health check: $1, passing blindly"
   exit 0
 fi
-
