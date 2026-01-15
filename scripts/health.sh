449cfa6c35a7a3365d46d69d1d1a40b7baa15fac
mhdbk <50269616+mhdbk@users.noreply.github.com>
2026-01-15 18:09:12 +0530

    syntax error fix

--- scripts/health.sh
+++ scripts/health.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 set -euo pipefail
 
-if [[ $1 == "celery"]]; then
+if [[ $1 == "celery" ]]; then
   pgrep -f 'celery.*worker' && pgrep -f 'celery.*beat' && celery -A core inspect ping -t 10 || exit 1
 elif [ $1 == "server" ]; then
   curl -sSf http://localhost:$PORT/health/ || exit 1
